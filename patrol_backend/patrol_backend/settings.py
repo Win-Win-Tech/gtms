5b8518898b2e6503abf375b03b356e2b832c2f4d
Five Y Technologies <142930108+Win-Win-Tech@users.noreply.github.com>
2025-09-20 12:51:40 +0530

    add_lat_log_v0.1

--- patrol_backend/patrol_backend/settings.py
+++ patrol_backend/patrol_backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-0f4$v*^k&wwuw0!dio!xgd&b(-d_fsre_=kcshan2gr@b9a^#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '147.93.27.224']
 
 # Application definition
 
