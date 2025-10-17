f611e90a755295dd07313af88755dc1f1e2c80e2
Five Y Technologies <142930108+Win-Win-Tech@users.noreply.github.com>
2025-10-17 23:11:15 +0530

    Twlio

--- patrol_backend/patrol_backend/settings.py
+++ patrol_backend/patrol_backend/settings.py
@@ -174,10 +174,14 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Twilio credentials
+TWILIO_SID = 'AC2f6c622ec954be8536e4b4ae1a913e90'
+TWILIO_AUTH_TOKEN = '8c0b864a3d79371cbbdb9571c4c8964b'
+
 # Twilio phone numbers
 TWILIO_PHONE = '+12543453221'  # Twilio voice-enabled number
 TWILIO_WHATSAPP_NUMBER = '+14155238886'  # Twilio sandbox WhatsApp number
 
 # Admin contact
-ADMIN_PHONE = '+918940996408'  # Admin's mobile number for voice call
-ADMIN_WHATSAPP = '+918940996408'  # Admin's WhatsApp number
+ADMIN_PHONE = '+918946066577'  # Admin's mobile number for voice call
+ADMIN_WHATSAPP = '+918946066577'  # Admin's WhatsApp number
