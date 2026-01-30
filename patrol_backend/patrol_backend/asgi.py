a3f5e72c825512fe7efd745ddfc72d1f574493d4
PitchaiBabu-M <mbabu2001@gmail.com>
2026-01-31 00:41:28 +0530

    live

--- patrol_backend/patrol_backend/asgi.py
+++ patrol_backend/patrol_backend/asgi.py
@@ -15,12 +15,13 @@
 
 
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patrol_backend.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from patrol_backend.middleware.jwt_auth import JWTAuthMiddleware
 from livetracking.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patrol_backend.settings")
 
 django_asgi_app = get_asgi_application()  # ✅ MUST be first
 
